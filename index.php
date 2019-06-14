72f6a73b7aa5b8d1c89996a48f82cdeee44a76f3
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-06-14 13:45:14 +0200

    gefixed

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
   echo "Dit is nieuwe code voor de tweede commit";
   echo "Dit is de derde commit";
   echo "avatar the last airbender is best show e ver";
-?>
-echo "Hallo Git World!";
+  echo "Hallo Git World!";
 echo "Dit is nieuwe code voor de tweede commit";
 echo "Dit is de derde commit";
 echo "Dit wordt de vijfde commit";
@@ -34,20 +33,14 @@ echo "Hello There";
   echo "Dit is een vijftiende commit";
   echo "Dit is de zestiende commit";
   echo "Pull request";
-<<<<<<< HEAD
   echo "blood";
-<<<<<<< HEAD
-<<<<<<< HEAD
-  echo "heyy"
-?> 
-=======
-=======
+  echo "heyy";
+
   echo "Mijn naam is Arjan";
   echo "Dit is de volgende test";
->>>>>>> d82192e733893517d41f1ce9b877e775430b9932
 echo "241";
   echo "Something";
-=======
+
   echo "Hello There";
 echo "Hallo Git World!";
 echo "Dit is nieuwe code voor de tweede commit";
@@ -68,7 +61,6 @@ echo "Pull request";
 echo "Mijn naam is Arjan";
 echo "Dit is de volgende test";
 echo "Something";
->>>>>>> bc2f8ce29b874ddfd9089adcae531f8531b38dbf
 ?>
 
 <img src="./footoo/F.jpg">
@@ -125,10 +117,7 @@ echo "Something";
 
 // Hello there
 echo "Hello There";
-<<<<<<< HEAD
 ?>
 
 <img src="https://www.google.com/search?q=random+img&source=lnms&tbm=isch&sa=X&ved=0ahUKEwikluyG8ejiAhWQGewKHfiZCW4Q_AUIECgB&biw=1536&bih=754#imgrc=X6wYAJJ9uPhsKM:">
-=======
-?>
->>>>>>> bc2f8ce29b874ddfd9089adcae531f8531b38dbf
+
