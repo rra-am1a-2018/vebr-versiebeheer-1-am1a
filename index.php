a5094a06bcdabece39aba068a7c401a140aba434
IJuanTM <iwan2510@gmail.com>
2019-06-14 13:38:47 +0200

    yes

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-  echo "Hallo Git World!";
-  echo "Dit is nieuwe code voor de tweede commit";
-  echo "Dit is de derde commit";
-  echo "Dit wordt de vijfde commit";
-  echo "Dit is de zevende commit";
-  echo "Dit is de achtste commit";
-  echo "Dit is de negende commit";
-  echo "Dit is de tiende commit";
-  echo "Dit is de elfde commit";
-  echo "Dit is de twaalfde commit";
-  echo "Dit is de dertiende commit";
-  echo "Dit is de veertiende commit";
-  echo "dit is een commit";
-  echo "Dit is een vijftiende commit";
-  echo "Dit is de zestiende commit";
-  echo "Pull request";
-  echo "Hello There";
+echo "Hallo Git World!";
+echo "Dit is nieuwe code voor de tweede commit";
+echo "Dit is de derde commit";
+echo "Dit wordt de vijfde commit";
+echo "Dit is de zevende commit";
+echo "Dit is de achtste commit";
+echo "Dit is de negende commit";
+echo "Dit is de tiende commit";
+echo "Dit is de elfde commit";
+echo "Dit is de twaalfde commit";
+echo "Dit is de dertiende commit";
+echo "Dit is de veertiende commit";
+echo "dit is een commit";
+echo "Dit is een vijftiende commit";
+echo "Dit is de zestiende commit";
+echo "Pull request";
+echo "Hello There";
 echo "Hallo Git World!";
 echo "Dit is nieuwe code voor de tweede commit";
 echo "Dit is de derde commit";
@@ -38,6 +38,9 @@ echo "Something";
 ?>
 
 <img src="./footoo/F.jpg">
+
+<img src="./footoo/when does this end.png">
+
 <?php
 echo "Hallo Git World!";
 echo "Dit is nieuwe code voor de tweede commit";
