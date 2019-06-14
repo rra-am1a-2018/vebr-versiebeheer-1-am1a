24905aa02039a8c59c7c858850257e9ad58b9eec
IJuanTM <iwan2510@gmail.com>
2019-06-14 14:33:50 +0200

    sneee

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
-  echo "Hallo Git World!";
-  echo "Dit is nieuwe code voor de tweede commit";
-  echo "Dit is de derde commit";
-  echo "Dit wordt de vijfde commit";
-  echo "Dit is de zevende commit";
-  echo "Dit is de achtste commit";
-  echo "Dit is de negende commit";
-  echo "Dit is de tiende commit";
-  echo "Dit is de elfde commit";
-  echo "Dit is de twaalfde commit";
-  echo "Dit is de dertiende commit";
-  echo "Dit is de veertiende commit";
+echo "Hallo Git World!";
+echo "Dit is nieuwe code voor de tweede commit";
+echo "Dit is de derde commit";
+echo "Dit wordt de vijfde commit";
+echo "Dit is de zevende commit";
+echo "Dit is de achtste commit";
+echo "Dit is de negende commit";
+echo "Dit is de tiende commit";
+echo "Dit is de elfde commit";
+echo "Dit is de twaalfde commit";
+echo "Dit is de dertiende commit";
+echo "Dit is de veertiende commit";
 
-  echo "Dit is een vijftiende commit";
-  echo "Dit is de zestiende commit";
-  echo "Pull request";
+echo "Dit is een vijftiende commit";
+echo "Dit is de zestiende commit";
+echo "Pull request";
 
-  echo "test";
+echo "test";
 
-  echo "klere lijers";
-  echo "yeet";
+echo "klere lijers";
+echo "yeet";
 ?>
-<button onclick="window.location.href = './snake/snake.html'">Click Here</button>
+<button onclick="window.location.href = './snake/snake.html'">SPEEL SNAKE!!!</button>
 <img src="./footoo/pwooah.gif">
 <img src="./footoo/kind.png">
 <img src="./footoo/dontaskquestions.png">
