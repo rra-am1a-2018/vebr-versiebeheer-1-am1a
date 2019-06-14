8fe4ea11bb709b93ed2c088fb0db783223c70519
Olivier Giskes <oliviergiskes2000@gmail.com>
2019-06-14 13:02:10 +0200

    avatar

--- index.php
+++ index.php
@@ -2,17 +2,5 @@
   echo "Hallo Git World!";
   echo "Dit is nieuwe code voor de tweede commit";
   echo "Dit is de derde commit";
-  echo "Dit wordt de vijfde commit";
-  echo "Dit is de zevende commit";
-  echo "Dit is de achtste commit";
-  echo "Dit is de negende commit";
-  echo "Dit is de tiende commit";
-  echo "Dit is de elfde commit";
-  echo "Dit is de twaalfde commit";
-  echo "Dit is de dertiende commit";
-  echo "Dit is de veertiende commit";
-
-  echo "Dit is een vijftiende commit";
-  echo "Dit is de zestiende commit";
-  echo "Pull request";
+  echo "avatar the last airbender is best show ever";
 ?>
