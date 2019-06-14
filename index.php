cae71c784c32958e62f5f6e73877655e03629ddd
IJuanTM <iwan2510@gmail.com>
2019-06-14 14:31:15 +0200

    poep

--- index.php
+++ index.php
@@ -12,9 +12,6 @@
 <img src="./footoo/lk.png">
 <img src="./footoo/def.jpg">
 <img src="./footoo/posi.png">
-<<<<<<< HEAD
 <img src="./footoo/geert.jpg">
 <img src="./footoo/noice.gif">
-=======
 <img src="./footoo/geert.jpg">
->>>>>>> 9feb1a85d34c8509f983be9decf631e130c87f58
