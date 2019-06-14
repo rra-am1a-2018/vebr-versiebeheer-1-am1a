05f179113f63ebe46bfd0e0bb3a7ee938487fdf5
Boris Waterhout <bw.4am1a@gmail.com>
2019-06-14 13:49:01 +0200

    mert

--- index.php
+++ index.php
@@ -15,79 +15,13 @@
   echo "Dit is een vijftiende commit";
   echo "Dit is de zestiende commit";
   echo "Pull request";
-  echo "Hello There";
-echo "Hallo Git World!";
-echo "Dit is nieuwe code voor de tweede commit";
-echo "Dit is de derde commit";
-echo "Dit wordt de vijfde commit";
-echo "Dit is de zevende commit";
-echo "Dit is de achtste commit";
-echo "Dit is de negende commit";
-echo "Dit is de tiende commit";
-echo "Dit is de elfde commit";
-echo "Dit is de twaalfde commit";
-echo "Dit is de dertiende commit";
-echo "Dit is de veertiende commit";
-echo "dit is een commit";
-echo "Dit is een vijftiende commit";
-echo "Dit is de zestiende commit";
-echo "Pull request";
-echo "Mijn naam is Arjan";
-echo "Dit is de volgende test";
-echo "Something";
-?>
 
-<img src="./footoo/F.jpg">
-<?php
-echo "Hallo Git World!";
-echo "Dit is nieuwe code voor de tweede commit";
-echo "Dit is de derde commit";
-echo "Dit wordt de vijfde commit";
-echo "Dit is de zevende commit";
-echo "Dit is de achtste commit";
-echo "Dit is de negende commit";
-echo "Dit is de tiende commit";
-echo "Dit is de elfde commit";
-echo "Dit is de twaalfde commit";
-echo "Dit is de dertiende commit";
-echo "Dit is de veertiende commit";
-echo "dit is een commit";
-echo "Dit is een vijftiende commit";
-echo "Dit is de zestiende commit";
-echo "Pull request";
-echo "heyy";
-
-echo "Mijn naam is Arjan";
-echo "Dit is de volgende test";
-
+  echo "heyy"
+?> 
+  echo "Mijn naam is Arjan";
+  echo "Dit is de volgende test";
+>>>>>>> d82192e733893517d41f1ce9b877e775430b9932
 echo "241";
-echo "Something";
-
-echo "Hallo Git World!";
-echo "Dit is nieuwe code voor de tweede commit";
-echo "Dit is de derde commit";
-echo "Dit wordt de vijfde commit";
-echo "Dit is de zevende commit";
-echo "Dit is de achtste commit";
-echo "Dit is de negende commit";
-echo "Dit is de tiende commit";
-echo "Dit is de elfde commit";
-echo "Dit is de twaalfde commit";
-echo "Dit is de dertiende commit";
-echo "Dit is de veertiende commit";
-echo "dit is een commit";
-echo "Dit is een vijftiende commit";
-echo "Dit is de zestiende commit";
-echo "Pull request";
-
-echo "Dit is de 17e commit";
-
-echo "heyy";
-echo "Mijn naam is Arjan";
-echo "Dit is de volgende test";
-echo "241";
-echo "Something";
-
-// Hello there
-echo "Hello There";
+  echo "Something";
 ?>
+>>>>>>> 656c27d6b2c99975cfabda438f1562b8ecd6891d
