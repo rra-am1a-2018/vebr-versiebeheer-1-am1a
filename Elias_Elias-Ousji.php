4253646651121b7737b272483147952eb83c9331
Elias Ousji <eliasousji01@gmail.com>
2019-06-21 14:10:32 +0200

    I'm back mfs

---
+++ Elias_Elias-Ousji.php
@@ -0,0 +1,30 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Eloias</td>
+      <td>Ousji</td>
+      <td>mf</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Hoe gaat Hallo  Hallo het?</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Help </td>
+      <td>Ik heb geen idee wat ikd oe</td>
+      <td>HO</td>
+    </tr>
+  </tbody>
+</table>
